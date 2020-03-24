d8c5402157c85034e12c058fee1b63a027562a3b
Stone Powell <spowell11@gmail.com>
2020-03-24 19:22:53 -0400

    initial push

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,40 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Remove all rows so we can display all data from scratch based on user input 
+d3.selectAll("td").remove();
+
+tableData.forEach((ufo) => 
+  {
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(([key, value]) => 
+      {
+        var cell = row.append("td");
+          cell.text(value);
+      });
+  });
+
+  function myFunction() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("datetime");
+    filter = input.value;
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+  
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
